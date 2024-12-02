64169d6640d2dbbabd754d74114569b025398bcf
useroz <a3521110344@alumno.uttehuacan.edu.mx>
2024-12-02 16:59:27 -0600

    Descripción de los cambios realizados

--- service-worker.js
+++ service-worker.js
@@ -5,50 +5,36 @@ const urlsToCache = [
   "/styles.css",
   "/script.js",
   "/manifest.json",
-  "/image/manzana_roja.jpg",
-  "/image/sandia.webp",
-  "/image/lechuga.webp",
-  "/image/platano.jpg",
-  "/image/tomate.avif",
-  "/image/zanahoria.jpg",
-  "/image/pera.jpg",
-  "/image/uva.png",
-  "/image/mango.jpg",
-  "/image/Naranja.webp",
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png"
 ];
 
-self.addEventListener("install", (event) => {
+self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
+      .then(cache => cache.addAll(urlsToCache))
       .then(() => self.skipWaiting())
   );
 });
 
-self.addEventListener("activate", (event) => {
+self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then((cacheNames) =>
-      Promise.all(
-        cacheNames.map((cache) => {
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
           if (cache !== CACHE_NAME) {
             return caches.delete(cache);
           }
         })
-      )
-    ).then(() => self.clients.claim())
+      );
+    }).then(() => self.clients.claim())
   );
 });
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request).catch(() => {
-        if (event.request.mode === "navigate") {
-          return caches.match("/offline.html");
-        }
-      });
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
 });
